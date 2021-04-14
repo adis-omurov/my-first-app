1118cc77030f13576cdf3b69b4feb367f41ad9c0
adis omurov <adisomurov003@gmail.com>
2021-04-14 14:07:56 +0600

    files in components

--- src/components/Big/Big.js
+++
@@ -1,9 +0,0 @@
-import classes from "./Big.module.css";
-const Big = ({ children }) => {
-    return (<div>
-        <button className={classes.button}>{children}</button>
-        </div>);
-}
-
-
-export default Big;
--- src/components/Button/Seventhmarth/Seventh.marth.js
+++
@@ -1,9 +0,0 @@
-import classes from "./Seventhmarth.module.css";
-const Button = ({ children }) => {
-    return (<div>
-        <button className={classes.button}>{children}</button>
-        </div>);
-}
-
-
-export default Seventhmarth;
