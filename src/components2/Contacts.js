bf06a083d257254f523dc785a1cbd7f774ddf981
adis omurov <adisomurov003@gmail.com>
2021-04-14 16:05:24 +0600

    Chaiging in componets

---
+++ src/components2/Contacts.js
@@ -0,0 +1,11 @@
+import classes from "./Contacts.module.css";
+
+const Contacts = () => {
+    return (
+        <div className={classes.Contacts}>
+            <h1>Market</h1>
+        </div>
+    );
+}
+
+export default Contacts;
---
+++ src/components2/Example.js
@@ -0,0 +1,11 @@
+import classes from "./Example.module.css";
+
+const Example = () => {
+    return (
+        <div className={classes.Example}>
+            <h1>Magazine</h1>
+        </div>
+    );
+}
+
+export default Example;
